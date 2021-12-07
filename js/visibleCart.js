97df8bc80e61840e3a3152c93a2777d2cd56ba45
unlocker1205 <19130215@st.hcmuaf.edu.vn>
2021-12-07 14:13:17 +0700

    view product

--- js/visibleCart.js
+++ js/visibleCart.js
@@ -1,29 +1,25 @@
-let cart = document.querySelector('#header li a[href*="Cart.html"]')
+let cart = document.querySelector('#header li a[href*="cart.html"]')
 let hoverCart = document.getElementById('cart-hover')
-// cart.onmouseover = function () {
-//     hoverCart.style.display = 'flex'
-// }
-// hoverCart.style.display = 'none'
-
-
-$(function(){
-    $(cart).hover(function(){
-        $(hoverCart).css('display','flex');
+if ($(window).width() > 1024) {
+    $(function () {
+        $(cart).hover(function () {
+            $(hoverCart).css('display', 'flex');
+        });
     });
-});
 
-$(function(){
-    $('#li-cart').hover(function(){
-        $(hoverCart).css('display','flex');
-    },function(){
-        $(hoverCart).css('display','none');
+    $(function () {
+        $('#li-cart').hover(function () {
+            $(hoverCart).css('display', 'flex');
+        }, function () {
+            $(hoverCart).css('display', 'none');
+        });
     });
-});
 
-$(function(){
-    $('#cart-hover').hover(function(){
-        $(hoverCart).css('display','flex');
-    },function(){
-        $(hoverCart).css('display','none');
+    $(function () {
+        $('#cart-hover').hover(function () {
+            $(hoverCart).css('display', 'flex');
+        }, function () {
+            $(hoverCart).css('display', 'none');
+        });
     });
-});
+}
